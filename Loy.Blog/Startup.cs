ef95b37a4d2539b1f735a702750698e83c863395
B.Tajahamdi <btajahmadi@hotmail.com>
2018-08-29 20:38:44 +0430

    New project added

--- Loy.Blog/Startup.cs
+++ Loy.Blog/Startup.cs
@@ -15,7 +15,6 @@ namespace Loy.Blog
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,12 +25,10 @@ namespace Loy.Blog
                 app.UseDeveloperExceptionPage();
             }
 
-
-            app.UseMvcWithDefaultRoute();
-            //app.Run(async (context) =>
-            //{
-            //    await context.Response.WriteAsync("Hello World!");
-            //});
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
